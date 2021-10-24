3065ab68a68d495546a436224411d292c83d9c87
yx <yx@ycgrandbeauty.com>
2021-10-24 14:56:28 +0800

    text3

--- src/main/java/org/example/text1.java
+++ src/main/java/org/example/text1.java
@@ -4,5 +4,8 @@ public class text1 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello 2222222222222222222");
+        for (int i = 0;i < 10;i++){
+            System.out.println("memeda");
+        }
     }
 }
